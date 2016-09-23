7dd3f2cffba399d1d60e5670b776d7ee1799f7a6
Mitiin Well <thinkweiwei@msn.com>
2016-09-23 11:58:56 +0800

    3.2TDD测试案例

--- src/cn/daboy/demo/DriverTest.java
+++ src/cn/daboy/demo/DriverTest.java
@@ -1,7 +1,26 @@
 package cn.daboy.demo;
 
+
+import junit.framework.TestCase;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Test;
+
 /**
  * Created by Administrator on 2016/9/23.
  */
 public class DriverTest extends TestCase {
+    Mockery context = new JUnit4Mockery();
+    @Test
+    public void testDriver() {
+        final ICar car = context.mock(ICar.class);
+        IDriver driver = new Driver();
+        context.checking(new Expectations(){
+            {
+                oneOf(car).run();
+            }
+        });
+        driver.driver(car);
+    }
 }
