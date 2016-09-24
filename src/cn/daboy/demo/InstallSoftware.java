6e4227871cc14e65686e6f8bc0d9b034871e55a9
Mitiin Well <thinkweiwei@msn.com>
2016-09-24 16:39:43 +0800

    重构解决与朋友类耦合度太高问题

--- src/cn/daboy/demo/InstallSoftware.java
+++ src/cn/daboy/demo/InstallSoftware.java
@@ -5,16 +5,6 @@ package cn.daboy.demo;
  */
 public class InstallSoftware {
     public void installWizard(Wizard wizard) {
-        int first = wizard.first();
-        if (first > 50) {
-            int second = wizard.second();
-            if (second > 50) {
-                int third = wizard.third();
-
-                if (third > 50) {
-                    wizard.first();
-                }
-            }
-        }
+        wizard.installWizard();
     }
 }
--- src/cn/daboy/demo/Wizard.java
+++ src/cn/daboy/demo/Wizard.java
@@ -7,16 +7,28 @@ import java.util.Random;
  */
 public class Wizard {
     private Random rand = new Random(System.currentTimeMillis());
-    public int first() {
+    private int first() {
         System.out.println("执行第一个方法...");
         return rand.nextInt(100);
     }
-    public int second() {
+    private int second() {
         System.out.println("执行第二个方法...");
         return rand.nextInt(100);
     }
-    public int third() {
+    private int third() {
         System.out.println("执行第三个方法...");
         return rand.nextInt(100);
+    }
+    public void installWizard() {
+        int first = this.first();
+        if (first > 50) {
+            int second = this.second();
+            if (second > 50) {
+                int third = this.third();
+                if (third > 50) {
+                    this.first();
+                }
+            }
+        }
     }
 }
