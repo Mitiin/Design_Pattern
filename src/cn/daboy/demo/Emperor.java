b61e98673d0eb6ce13e0ca323c6732c7341aa832
Mitiin Well <thinkweiwei@msn.com>
2016-09-26 17:42:20 +0800

    多例模式

--- src/cn/daboy/demo/Emperor.java
+++ src/cn/daboy/demo/Emperor.java
@@ -1,17 +1,33 @@
 package cn.daboy.demo;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by Administrator on 2016/9/26.
  */
 public class Emperor {
-    private static final Emperor emperor = new Emperor();
-    private Emperor() {
-
+    private static int maxNumOfEmperor = 2;
+    private static ArrayList<String> nameList = new ArrayList<String>();
+    private static ArrayList<Emperor> emperorList = new ArrayList<Emperor>();
+    private static int countNumOfEmperor = 0;
+    static {
+        for (int i = 0; i < maxNumOfEmperor;i++) {
+            emperorList.add(new Emperor("皇"+(i+1)+"帝"));
+        }
+    }
+    private Emperor(){};
+    private Emperor(String name) {
+        nameList.add(name);
     }
     public static Emperor getInstance() {
-        return emperor;
+        Random random = new Random();
+        countNumOfEmperor = random.nextInt(maxNumOfEmperor);
+        return emperorList.get(countNumOfEmperor);
     }
-    public static void say() {
-        System.out.println("我就是皇帝某某某...");
+
+    public void say() {
+        System.out.println(nameList.get(countNumOfEmperor));
     }
+
 }
