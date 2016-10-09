776caa8128345ef2b3dd0bc4b8aed55887d71dd2
Mitiin Well <thinkweiwei@msn.com>
2016-10-09 15:12:32 +0800

    原型模式

--- src/cn/daboy/demo/Client.java
+++ src/cn/daboy/demo/Client.java
@@ -2,29 +2,38 @@ package cn.daboy.demo;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
+import java.util.Random;
 
 /**
  * Created by Administrator on 2016/9/29.
  */
 public class Client {
+    private static int MAX_COUNT = 6;
     public static void main(String[] args) {
-        //定义一个痴迷的玩家
-        IGamePlayer player = new GamePlayer("张三");
-        //定义一个hanlder
-        InvocationHandler handler = new GamePlayIH(player);
-        //开始打游戏，记下时间戳
-        System.out.println("开始时间是：2009-8-25 10:45");
-        //获得类的class loader
-        ClassLoader cl = player.getClass().getClassLoader();
-        //动态产生一个代理者
-        IGamePlayer proxy = (IGamePlayer)Proxy.newProxyInstance(cl,new Class[]{IGamePlayer.class},handler);
-        //登录
-        proxy.login("zhangSan", "password");
-        //开始杀怪
-        proxy.killBoss();
-        //升级
-        proxy.upgrade();
-        //记录结束游戏时间
-        System.out.println("结束时间是：2009-8-26 03:40");
+        int i = 0;
+        Mail mail = new Mail(new AdvTemplate());
+        mail.setTail("XX银行版权所有");
+        while(i < MAX_COUNT) {
+            Mail cloneMail = mail.clone();
+            cloneMail.setAppellation(getRandString(5)+"先生(女士)");
+            cloneMail.setReceiver(getRandString(5)+"@"+getRandString(8)+".com");
+            sendMail(cloneMail);
+            i++;
+        }
     }
+
+    public static void sendMail(Mail mail) {
+        System.out.println("标题:"+mail.getSubject()+"\t收件人:"+mail.getReceiver()+"\t...发送成功!");
+    }
+
+    public static String getRandString(int maxLength) {
+        String source = "abcdefghijklmnopqrstuvwxyz";
+        StringBuffer sb = new StringBuffer();
+        Random rand = new Random();
+        for (int i = 0;i<maxLength; i++) {
+            sb.append(source.charAt(rand.nextInt(source.length())));
+        }
+        return sb.toString();
+    }
+
 }
