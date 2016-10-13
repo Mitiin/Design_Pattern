0b44ab4a4298764cb95bf7bc43c22299d09796e5
Mitiin Well <thinkweiwei@msn.com>
2016-10-13 09:43:24 +0800

    迭代器模式

--- src/cn/daboy/demo/Client.java
+++ src/cn/daboy/demo/Client.java
@@ -2,6 +2,7 @@ package cn.daboy.demo;
 
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -9,14 +10,19 @@ import java.util.Arrays;
  */
 public class Client {
     public static void main(String[] args) {
-        IOuterUserBaseInfo baseInfo = new OuterUserBaseInfo();
-        IOuterUserHomeInfo homeInfo = new OuterUserHomeInfo();
-        IOuterUserOfficeInfo officeInfo = new OuterUserOfficeInfo();
-
-        IUserInfo youngGirl = new OuterUserInfo(baseInfo,homeInfo,officeInfo);
-        for (int i = 0;i < 101;i++) {
-            youngGirl.getMobileNumber();
+        IProject project = new Project();
+        project.add("星球大战项目ddddd",10,100000);
+        project.add("扭转时空项目",100,100000000);
+        project.add("超人改造项目",10000,1000000000);
+        for (int i = 0;i < 104;i++) {
+            project.add("第"+i+"个项目",i*5,i*1000000);
         }
+        IProjectItertor projectItertor = project.iterator();
+        while (projectItertor.hasNext()) {
+            IProject p = (IProject)projectItertor.next();
+            System.out.println(p.getProjectInfo());
+        }
+
     }
 
 }
---
+++ src/cn/daboy/demo/IProjectItertor.java
@@ -0,0 +1,10 @@
+package cn.daboy.demo;
+
+import java.util.Iterator;
+
+/**
+ * Created by Administrator on 2016/10/12.
+ */
+public interface IProjectItertor extends Iterator {
+
+}
