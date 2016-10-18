c41b9817503fac9c3857420a0d3f295b42b9ba47
Mitiin Well <thinkweiwei@msn.com>
2016-10-18 11:22:06 +0800

    享元模式

--- src/cn/daboy/demo/Client.java
+++ src/cn/daboy/demo/Client.java
@@ -11,40 +11,14 @@ import java.util.HashMap;
  */
 
 public class Client {
-
-    //运行四则运算
     public static void main(String[] args) throws IOException{
-        String expStr = getExpStr();
-        //a+b-c
-        //赋值
-        HashMap<String,Integer> var = getValue(expStr);
-        //{b=2, c=1, a=5}
-
-        Calculator cal = new Calculator(expStr);
-        System.out.println("运算结果为："+expStr +"="+cal.run(var));
-    }
-
-    //获得表达式
-    public static String getExpStr() throws IOException{
-        System.out.print("请输入表达式：");
-        return (new BufferedReader(new InputStreamReader(System.in))).readLine();
-    }
-
-    //获得值映射
-    public static HashMap<String,Integer> getValue(String exprStr) throws IOException{
-        HashMap<String,Integer> map = new HashMap<String,Integer>();
-
-        //解析有几个参数要传递
-        for(char ch:exprStr.toCharArray()){
-            if(ch != '+' && ch != '-'){
-                if(!map.containsKey(String.valueOf(ch))){ //解决重复参数的问题
-                    System.out.print("请输入"+ch+"的值:");
-                    String in = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-                    map.put(String.valueOf(ch),Integer.valueOf(in));
-                }
+        for (int i = 0;i < 4;i++) {
+            String subject = "科目"+i;
+            for (int j = 0;j < 30;j++) {
+                String key = subject+"考试地点"+j;
+                SignInfoFactory.getSignInfo(key);
             }
         }
-
-        return map;
+        SignInfo signInfo = SignInfoFactory.getSignInfo("科目1考试地点1");
     }
 }
